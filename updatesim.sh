21688ad5dfd5e544995f3005f029c958742dfec6
root <root@ip-172-31-21-224.us-east-2.compute.internal>
2018-07-17 00:19:42 +0300

    add apikey directory

--- updatesim.sh
+++ updatesim.sh
@@ -2,6 +2,6 @@
 
 cd /home/ubuntu/ShadowBot/simc_bfa/simc && git pull
 cd /home/ubuntu/ShadowBot/simc_bfa/simc/engine/
-make optimized
+make OPENSSL=1 optimized
 /bin/cp -f /home/ubuntu/ShadowBot/simc_bfa/simc/engine/simc /home/ubuntu/ShadowBot/output/
 echo date " update complete!" >> /home/ubuntu/ShadowBot/update_simc.log
