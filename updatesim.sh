682fc074a3b887ea0dac29b4aed3a01404e5d631
root <root@ip-172-31-21-224.us-east-2.compute.internal>
2018-07-24 00:53:02 +0300

    gitignore

--- updatesim.sh
+++ updatesim.sh
@@ -3,5 +3,5 @@
 cd /home/ubuntu/ShadowBot/simc_bfa/simc && git pull
 cd /home/ubuntu/ShadowBot/simc_bfa/simc/engine/
 make OPENSSL=1 optimized
-/bin/cp -f /home/ubuntu/ShadowBot/simc_bfa/simc/engine/simc /home/ubuntu/ShadowBot/output/
-echo date " update complete!" >> /home/ubuntu/ShadowBot/update_simc.log
+/bin/cp -f /home/ubuntu/ShadowBot/simc_bfa/simc/engine/simc /home/ubuntu/ShadowBot/output/simc
+echo $(date) " update complete!" >> /home/ubuntu/ShadowBot/update_simc.log
